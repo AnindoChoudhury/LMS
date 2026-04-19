45d4ecc16bfa1b0803bad54a8ef0449a6dac60f2
AnindoChoudhury <anindo.choudhury2004@gmail.com>
2026-04-19 21:48:10 +0530

    Wrote the apis

---
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Course;
+import com.example.demo.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    @Autowired
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    // GET Request: http://localhost:8080/api/courses
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllCourses() {
+        List<Course> courses = courseService.getAllCourses();
+        return ResponseEntity.ok(courses);
+    }
+
+    // POST Request: http://localhost:8080/api/courses
+    @PostMapping
+    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+        Course savedCourse = courseService.createCourse(course);
+        return ResponseEntity.ok(savedCourse);
+    }
+}
